4c4c17ae0f7c7c9cdad56c89b2c35f2be0424133
Pablo Ortiz <pablomiguelortiz07@gmail.com>
2022-06-21 09:34:54 -0300

    se sube la mayoria de los insert

--- script_creacion_bi.sql
+++ script_creacion_bi.sql
@@ -32,7 +32,7 @@ CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_neumatico_tipo
 
 CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_sector_tipo
 (
-	sector_incidente_codigo INTEGER IDENTITY(1,1) PRIMARY KEY,
+	sector_tipo_codigo INTEGER IDENTITY(1,1) PRIMARY KEY,
 	sector_tipo_descripcion nvarchar(255)
 )
 
@@ -60,7 +60,7 @@ CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_escuderia
 CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_circuito
 (
 	circuito_codigo INTEGER IDENTITY(1,1) PRIMARY KEY,
-	circutio_nombre nvarchar(255),
+	circuitio_nombre nvarchar(255),
 	circuito_pais integer
 )
 
@@ -126,7 +126,7 @@ CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_caja_de_cambio_medicion
 (
 	caja_medicion_codigo INTEGER IDENTITY(1,1) PRIMARY KEY,
 	caja_medicion_medicion decimal (18,0),
-	caja_medicion_neumatico_numero_serie nvarchar(255),
+	caja_medicion_caja_numero_serie nvarchar(255),
 	caja_medicion_temperatura_aceite decimal(18,2),
 	caja_medicion_rpm decimal(18,2),
 	caja_medicion_desgaste decimal(18,2)
@@ -153,6 +153,252 @@ CREATE TABLE  [NOCURSOMASLOSSABADOS].dim_parada_box
 	parada_numero_vuelta decimal(18,0),
 	parada_tiempo decimal(18,2),
 )
+
+
+
+/************************************/
+/*			CARGA  BI				*/
+/************************************/
+
+
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_sector_tipo
+(
+	sector_tipo_codigo,
+	sector_tipo_descripcion
+)
+SELECT 
+	sector_tipo_codigo,
+	sector_tipo_descripcion 
+FROM [NOCURSOMASLOSSABADOS].Sector_Tipo
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_sector
+(
+	sector_codigo,
+	sector_circuito,
+	sector_distancia,
+	sector_tipo
+)
+SELECT 
+	sector_codigo,
+	sector_circuito,
+	sector_distancia,
+	sector_tipo
+FROM [NOCURSOMASLOSSABADOS].Sector
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_incidente
+(
+	incidente_codigo,
+	incidente_sector,
+	incidente_carrera,
+	incidente_tiempo,
+	incidente_bandera
+)
+SELECT 
+	incidente_codigo,
+	incidente_sector,
+	incidente_carrera,
+	incidente_tiempo,
+	incidente_bandera
+FROM [NOCURSOMASLOSSABADOS].Incidente
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_incidente_tipo
+(
+	incidente_tipo_codigo,
+	incidente_tipo_descripcion
+)
+SELECT 
+	incidente_tipo_codigo,
+	incidente_tipo_descripcion
+FROM [NOCURSOMASLOSSABADOS].Incidente_Tipo
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_circuito
+(
+	circuito_codigo,
+	circuitio_nombre,
+	circuito_pais
+)
+SELECT 
+	circuito_codigo,
+	circuito_nombre,
+	circuito_pais
+FROM [NOCURSOMASLOSSABADOS].Circuito
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_neumatico_tipo
+(
+	neumatico_tipo_codigo,
+	neumatico_tipo_descripcion
+)
+SELECT 
+	neumatico_tipo_codigo,
+	neumatico_tipo_descripcion
+FROM [NOCURSOMASLOSSABADOS].Neumatico_Tipo
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_sector_tipo
+(
+	sector_tipo_codigo,
+	sector_tipo_descripcion
+)
+SELECT 
+	sector_tipo_codigo,
+	sector_tipo_descripcion
+FROM [NOCURSOMASLOSSABADOS].sector_Tipo
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_auto
+(
+	auto_codigo,
+	auto_escuderia,
+	auto_piloto,
+	auto_modelo,
+	auto_numero
+)
+SELECT 
+	auto_codigo,
+	auto_escuderia,
+	auto_piloto,
+	auto_modelo,
+	auto_numero
+FROM [NOCURSOMASLOSSABADOS].Auto
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_piloto
+(
+	piloto_codigo,
+	piloto_nombre,
+	piloto_apellido,
+	piloto_pais,
+	piloto_fecha_nacimiento
+)
+SELECT 
+	piloto_codigo,
+	piloto_nombre,
+	piloto_apellido,
+	piloto_pais,
+	piloto_fecha_nacimiento
+FROM [NOCURSOMASLOSSABADOS].Piloto
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_freno_medicion
+(
+	freno_medicion_codigo,
+	freno_medicion_medicion,
+	freno_medicion_freno_numero_serie,
+	freno_medicion_grosor,
+	freno_medicion_temperatura
+)
+SELECT 
+	freno_medicion_codigo,
+	freno_medicion_medicion,
+	freno_medicion_freno_numero_serie,
+	freno_medicion_grosor,
+	freno_medicion_temperatura
+FROM [NOCURSOMASLOSSABADOS].Freno_medicion
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_neumatico_medicion
+(
+	neumatico_medicion_codigo,
+	neumatico_medicion_medicion,
+	neumatico_medicion_neumatico_numero_serie,
+	neumatico_medicion_profundidad,
+	neumatico_medicion_temperatura
+)
+SELECT 
+	neumatico_medicion_codigo,
+	neumatico_medicion_medicion,
+	neumatico_medicion_neumatico_numero_serie,
+	neumatico_medicion_profundidad,
+	neumatico_medicion_temperatura
+FROM [NOCURSOMASLOSSABADOS].Neumatico_medicion
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_auto_carrera
+(
+	auto_carrera_codigo,
+	auto_carrera_auto,
+	auto_carrera_carrera
+)
+SELECT 
+	auto_carrera_codigo,
+	auto_carrera_auto,
+	auto_carrera_carrera
+FROM [NOCURSOMASLOSSABADOS].Auto_Carrera
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_medicion
+(
+	medicion_codigo,
+	medicion_auto_carrera,
+	medicion_sector,
+	medicion_numero_vuelta,
+	medicion_distancia_carrera,
+	medicion_distancia_vuelta,
+	medicion_posicion,
+	medicion_velocidad,
+	medicion_combustible
+)
+SELECT 
+	medicion_codigo,
+	medicion_auto_carrera,
+	medicion_sector,
+	medicion_numero_vuelta,
+	medicion_distancia_carrera,
+	medicion_distancia_vuelta,
+	medicion_posicion,
+	medicion_velocidad,
+	medicion_combustible
+FROM [NOCURSOMASLOSSABADOS].Medicion
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_caja_de_cambio_medicion
+(
+	caja_medicion_codigo,
+	caja_medicion_medicion,
+	caja_medicion_caja_numero_serie,
+	caja_medicion_temperatura_aceite,
+	caja_medicion_desgaste
+)
+SELECT 
+	caja_medicion_codigo,
+	caja_medicion_medicion,
+	caja_medicion_caja_numero_serie,
+	caja_medicion_temperatura_aceite,
+	caja_medicion_desgaste
+FROM [NOCURSOMASLOSSABADOS].Caja_De_Cambio_Medicion
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_motor_medicion
+(
+	motor_medicion_codigo,
+	motor_medicion_medicion,
+	motor_medicion_motor_numero_serie,
+	motor_medicion_potencia,
+	motor_medicion_temperatura_aceite,
+	motor_medicion_temperatura_agua,
+	motor_medicion_rpm
+)
+SELECT 
+	motor_medicion_codigo,
+	motor_medicion_medicion,
+	motor_medicion_motor_numero_serie,
+	motor_medicion_potencia,
+	motor_medicion_temperatura_aceite,
+	motor_medicion_temperatura_agua,
+	motor_medicion_rpm
+FROM [NOCURSOMASLOSSABADOS].Motor_Medicion
+
+
+INSERT INTO [NOCURSOMASLOSSABADOS].dim_parada_box
+(
+	parada_codigo,
+	parada_auto_carrera,
+	parada_numero_vuelta,
+	parada_tiempo
+)
+SELECT 
+	parada_codigo,
+	parada_auto_carrera,
+	parada_numero_vuelta,
+	parada_tiempo
+FROM [NOCURSOMASLOSSABADOS].Parada_Box
 
 
 
